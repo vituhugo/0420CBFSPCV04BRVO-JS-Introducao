355bb391c868886f5bb46122b3dc34f1a9e7aa0e
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-09-09 19:49:08 -0300

    ajuste exericcio

--- desafio/script.js
+++ desafio/script.js
@@ -1,43 +1,45 @@
 function mostrarAlerta () {
-    // Retirar o estilo display:none do alerta-1
+    // TODO: Retirar o estilo display:none do alerta-1
 }
 
 function mostrarAlertaComShow() {
-    // Adicionar classe show no alerta-2
-    //Para executar essa função clique no botão "Alerta bonitinho"
+    // TODO: Adicionar classe show no alerta-2
+    //       Para executar essa função clique no botão "Alerta bonitinho"
 }
 
 function desapareceComOAlerta() {
-    // Remover a classe show do alerta-2
+    // TODO: Remover a classe show do alerta-2
     // 
 }
 
 function adicionar() {
-    // Captura os valores dos dois campos ( através da propriedade "value" de um elemento input )
-    // E soma-los
+    // TODO: Somar os campos que forem adicionados na calculadora e
+    //       exibir no conteúdo do #resultado(elemento HTML com id resultado)
     document.getElementById('resultado').innerHTML = '?';
 }
 
 function subtrair() {
-    // Captura os valores dos dois campos ( através da propriedade "value" de um elemento input )
-    // E subtrai-los
+    // TODO: Subtrair os campos que forem preenchidos na calcualdora e 
+    //       exibir no conteúdo do #resultado(elemento HTML com id resultado)
     document.getElementById('resultado').innerHTML = '?';
 }
 
 function multiplicar() {
-    // Captura os valores dos dois campos ( através da propriedade "value" de um elemento input )
-    // E multiplica-los
+    // Multiplicar e exibir no resultado
     document.getElementById('resultado').innerHTML = '?';
 }
 
 function dividir() {
-    // Captura os valores dos dois campos ( através da propriedade "value" de um elemento input )
-    // E dividi-los
+    // Dividir e exibir no resultado
     document.getElementById('resultado').innerHTML = '?';
 }
 
 function elevar() {
     // Captura os valores dos dois campos ( através da propriedade "value" de um elemento input )
-    // E elevar a potencia do segundo ( Tentei dar uma googlada, vao precisar usar o objeto "Math" )
+    // E elevar o primeiro a potência do segundo ( Tentei dar uma googlada, vao precisar usar o objeto "Math" )
+    //
+    // Exemplo: value_1 = 10, value_2 = 2
+    // 10² = 100
+    
     document.getElementById('resultado').innerHTML = '?';
 }
