28ebda7ab8ee02be6759df8c16f89aa03d32fdd2
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-09-09 15:12:45 -0300

    first commit

---
+++ exemplo/script.js
@@ -0,0 +1,37 @@
+//Variaveis $
+// $variavel = "VALOR";
+let variavel = "VALOR";
+
+//Cosntantes define("CONSTANTE", "VALOR");
+const CONSTANTE = "VALOR"
+
+//Funcoes function nomeDaFuncao() {}
+function nomeDaFuncao(arg1, arg2) {
+    return true;
+}
+
+//Laço de repetição
+for (let i = 0 ; i < 10 ; i++) {
+    //Código do laço
+}
+//Array = [PHP] ARRAY ENUMERATIVO
+let lista = [
+    "Banana",
+    "maçã",
+    "cogumelo"
+];
+
+//Objetos Literais $objeto = ["chave" => "VALOR"]
+let objeto = {
+    "chave": "VALOR"
+}
+
+//Acessando Arrays: VARIAVEL[CHAVE]
+lista[0]
+
+// FUNCOES DE NOTIFICACAO
+// alert [PHP]ECHO 
+alert("Ola usuário");
+
+// ESCRITA NO CONSOLE
+console.log("Olá desenvolvedor")
